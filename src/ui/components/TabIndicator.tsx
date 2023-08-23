e4905dfe51c5e36531a9964ccaa81b580cf2b73d
Gian <gian@nullbug.dev>
2023-08-22 21:17:17 -0300

    adjust

--- src/ui/components/TabIndicator.tsx
+++ src/ui/components/TabIndicator.tsx
@@ -1,24 +1,33 @@
 import { useEffect } from 'react'
 import { Animated, Dimensions } from 'react-native'
-import { ParamListBase, TabNavigationState } from '@react-navigation/native'
+import {
+  ParamListBase,
+  TabNavigationState,
+  useIsFocused,
+} from '@react-navigation/native'
 
 export const TabIndicator = ({
   state,
 }: {
   state: TabNavigationState<ParamListBase>
 }) => {
+  const isFocused = useIsFocused()
+
   const { width } = Dimensions.get('screen')
   const indicatorWidth = width / 2
-  const translateValue = new Animated.Value(width * 0)
-  const toValue = indicatorWidth + (state.index - 1) * indicatorWidth
+  const translateValue = new Animated.Value(0)
 
-  useEffect(() => {
+  const slide = () => {
     Animated.timing(translateValue, {
-      toValue,
+      toValue: indicatorWidth * state.index,
       duration: 300,
       useNativeDriver: true,
     }).start()
-  }, [state])
+  }
+
+  useEffect(() => {
+    slide()
+  }, [state, isFocused, slide])
 
   return (
     <Animated.View
